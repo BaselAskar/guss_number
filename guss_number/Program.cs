78b5627f1d75fc9f2cbc7d9d4b8c18a93096f2be
Basel Askar <baselaskar91@gmail.com>
2024-08-27 20:33:35 +0200

    Add project files.

---
+++ guss_number/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace guss_number
+{
+    internal class Program
+    {
+        static int RandomNumber()
+        {
+            Random rn = new Random();
+
+            return rn.Next(1, 100);
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            // Welcome message
+
+
+
+            // Create random number
+            int randomNumber = RandomNumber();
+
+
+
+            // User Insert the number
+
+
+
+
+
+            // Conditions of the game
+
+
+
+
+            // out the message of number is greater or smaller than the guss number
+        }
+    }
+}
